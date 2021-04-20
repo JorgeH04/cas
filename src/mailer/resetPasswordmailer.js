fb82ec75f9d2c5acb75d7ac0f72c38b747f8d523
Jorge <jhessle04@gmail.com>
2021-04-19 23:19:53 -0300

    J

---
+++ src/mailer/resetPasswordmailer.js
@@ -0,0 +1,24 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+// this is another way of exporting a method
+exports.resetPassword = (user,token) => {
+    //this the message body for mail
+    nodeMailer.transporter.sendMail({
+       from: 'passwordreset@demo.com',
+       to: user.email,
+       subject: 'Resetear contraseña',
+       text: 'Le estamos enviando este email porque usted ha solicitado cambiar la contraseña de su cuenta.\n\n' +
+          'Haga click en el siguiente link, o cópielo en su navegador para completar el proceso:\n\n' +
+         'https://' + 'www.cerveceriagrossi.com' + '/userpasswordForgot/' + token + '\n\n' +
+      //  'http://' + 'localhost:3000' + '/userpasswordForgot/' + token + '\n\n' +
+          'Si usted no solicitó este link, por favor ignore este email y su contraseña permanecerá tal como usted la creó.\n'
+    }, (err, info) => {
+        if (err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+        req.flash('success','Le hemos enviado un e-mail con una serie de instrucciones.');
+        return;
+    });
+}
---
+++ src/models/proddos.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema({
+  name: String,
+  title: String,
+  image: String,
+  imagedos: String,
+  imagetres: String,
+  imagecuatro: String,
+  imagecinco: String,
+  description: String,
+  filtroprice: String,
+  enstock:String,
+  color: String,
+  coloruno: String,
+  colordos: String,
+  colortres: String,
+  colorcuatro: String,
+  talle: String,
+  talleuno: String,
+  talledos: String,
+  talletres: String,
+  tallecuatro: String,
+  tallecinco: String,
+  talleseis: String,
+  oldprice: Number,
+  price: Number,
+  status: {
+    type: Boolean,
+    default: false
+  },
+  like: {
+    type: Boolean,
+    default: false
+  }
+
+});
+
+module.exports = mongoose.model('Proddos', NoteSchema);
